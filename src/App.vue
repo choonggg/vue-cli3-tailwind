387f25d3121299587c82d1ebc2bf21b8f93f91d6
Chong Hui <iamchoonggg@gmail.com>
2018-03-13 23:09:37 +0800

    setup tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container bg-black text-center">
+      <HelloWorld></HelloWorld>
+    </div>
   </div>
 </template>
 
@@ -17,12 +18,13 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+
+<style src="./styles/main.css"></style>
